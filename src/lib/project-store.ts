95407eb45622ffe77778ff212e7a632a8fcf0e4c
owjnavard <oojnavardcaspian.co@gmail.com>
2026-08-01 13:37:39 +0330

    Updated project-store.ts

--- src/lib/project-store.ts
+++ src/lib/project-store.ts
@@ -125,14 +125,7 @@ function makeElevators(projectId: string, count: number): Elevator[] {
   });
 }
 
-function seed(): {
-  projects: Project[];
-  elevators: Elevator[];
-  tasks: Task[];
-  commitments: Commitment[];
-  supplies: PartSupply[];
-  taskTemplates: TaskTemplate[];
-} {
+function seed(): { projects: Project[]; elevators: Elevator[] } {
   const projects: Project[] = [];
   const elevators: Elevator[] = [];
 
